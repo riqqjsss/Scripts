efbc8be39b13f07b006f8293b7f2a65a293591a8
riqqjsss <riqdiscord15@gmail.com>
2025-04-24 21:50:28 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+--Loader of Riq Hub Script!
+if game.PlaceId == "142823291" then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/riqqjsss/Scripts/refs/heads/main/MM2.lua'))()
+elseif game.PlaceId == "13772394625" then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/riqqjsss/Scripts/refs/heads/main/BL.lua'))()
+end
